32fc76699bfbdd34709fbe3f5ca8ca8784e6ced0
Prashant Patil <62135562+upworkprashantp@users.noreply.github.com>
2020-11-26 17:08:21 +0530

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,22 @@
+from sshtunnel import SSHTunnelForwarder
+from paramiko import SSHClient
+
+server = SSHTunnelForwarder(
+    'efg.hdfcbank.com',
+    ssh_username="CTRUENORTH",
+    ssh_password="Bank@123",
+    remote_bind_address=('127.0.0.1', 8080)
+)
+
+server.start()
+
+print(server.local_bind_port)  # show assigned local port
+# work with `SECRET SERVICE` through `server.local_bind_port`.
+with SSHClient() as ssh:
+    ssh.connect('efg.hdfcbank.com',
+                port=server.local_bind_port,
+                username='CTRUENORTH',
+                password='Bank@123',
+                )
+    ssh.exec_command('-d')
+server.stop()
